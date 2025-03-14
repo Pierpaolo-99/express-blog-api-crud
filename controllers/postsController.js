09633e8f6c01f9f2e1538fb117e9b53d8100b7f1
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-14 14:45:45 +0100

    logic implementation index, find by tag

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,8 +1,16 @@
 // import the data
+const { log } = require('console')
 const posts = require('../data/posts')
 
 function index (req, res) {
-    res.json(posts)
+
+    let filteredPosts = posts
+
+    if (req.query.tag){
+        filteredPosts = posts.filter(post => post.tags.includes(req.query.tag))
+    }
+    
+    res.json(filteredPosts)
 }
 
 function show (req,res) {
